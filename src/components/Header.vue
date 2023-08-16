67196dbec08e952255a33f18fbc9dd9767576956
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-08-16 08:53:40 +0430

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,7 +8,7 @@ const props = defineProps(['toggleSidebar'])
     <div class="bg-gray-300 w-full sticky top-0">
         <div class="flex justify-between items-center h-[50px] w-full p-4">
             <div 
-            class="bg-gray-950 h-full w-6"
+            class="bg-gray-950 h-full w-6 cursor-pointer"
             @click="toggleSidebar"
             ></div>
         </div>
