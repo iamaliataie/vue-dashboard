96f4057fa89396424b973bd6e4a7e14608b06792
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2023-08-20 05:29:33 +0430

    create navlink component

---
+++ src/components/NavLink.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+const props = defineProps(['link', 'icon', 'text'])
+
+</script>
+
+<template>
+    <RouterLink :to="{ name: link }"
+        class="py-4 flex items-center space-x-4 px-4"
+    >
+        <span class="material-symbols-outlined">{{ icon }}</span>
+        <span class="text-xl font-bold">{{ text }}</span>
+    </RouterLink>
+</template>
+
+<style scoped>
+
+</style>
