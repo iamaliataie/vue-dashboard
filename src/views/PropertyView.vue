27a19f55c83af3d06b296f7e503aa57b33b1ab5b
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2023-08-23 06:47:49 +0430

    update property view page

--- src/views/PropertyView.vue
+++ src/views/PropertyView.vue
@@ -7,7 +7,9 @@ import PropertyItem from '../components/PropertyItem.vue';
   <div>
     <h1 class="text-3xl font-bold">Property page</h1>
     <div class="py-2 rounded-md">
-          <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div 
+        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8"
+      >
         <PropertyItem />
       </div>
     </div>
