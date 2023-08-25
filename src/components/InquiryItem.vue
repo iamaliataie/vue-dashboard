dea30fe60e9262881faada744490e2692aaf418b
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2023-08-25 07:51:33 +0430

    create inquiry item component

---
+++ src/components/InquiryItem.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="shadow-lg p-6 rounded-md bg-white border">
+        <h1 class="text-3xl font-bold">Title of inquiry</h1>
+        <p class="text-xl font-semibold">Aug 23,2023</p>
+        <div class="flex justify-between items-center mt-6">
+            <p class="text-xl">Ali Ahmad Atie</p>
+            <small class="px-2 py-1 bg-gray-400 font-semibold rounded-sm">unread</small>
+        </div>
+    </div>
+</template>
+
+
+<style scoped></style>
