30f9dd0c76c3a5f6be19e0e08ee18395bff31af8
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2023-08-25 07:45:20 +0430

    update property item component

--- src/components/PropertyItem.vue
+++ src/components/PropertyItem.vue
@@ -3,15 +3,17 @@
 </script>
 
 <template>
-    <div class="bg-white overflow-hidden rounded-lg p-2 flex flex-col space-y-4 shadow-xl">
+    <div class="bg-white overflow-hidden rounded-lg p-2 flex flex-col space-y-4 shadow-xl border">
         <div class="w-full relative overflow-hidden rounded-sm bg-green-400">
             
             <img src="../assets/bookmark.png" alt="" class="w-full">
         </div>
-        <div class="w-full">
-            <small class=" bg-red-500 text-white px-2 py-1 rounded-sm">Commercial</small>
-            <h1 class="text-3xl">House One</h1>
-            <p class="text-2xl">400,000 AFN</p>
+        <div class="w-full flex flex-col space-y-4">
+            <small class="bg-red-500 text-white px-2 py-1 rounded-md text-lg w-fit">Commercial</small>
+            <div>
+                            <h1 class="text-3xl">House One</h1>
+                <p class="text-2xl">400,000 AFN</p>
+            </div>
         </div>
         <div class="w-full">
             <a href="#" class="block py-2 px-3 bg-blue-600 text-white text-xl lg:text-2xl text-center rounded-md">View Property</a>
