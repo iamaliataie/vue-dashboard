1a404e35e1e5ea1d9f844b1308873900bd0bc613
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2023-08-25 07:54:29 +0430

    add inquiry item component to inquiry page

--- src/views/InquiryView.vue
+++ src/views/InquiryView.vue
@@ -1,3 +1,7 @@
+<script setup>
+import InquiryItem from '../components/InquiryItem.vue';
+</script>
+
 <template>
     <div>
       <h1 class="text-3xl font-bold">Inquiries</h1>
@@ -5,7 +9,7 @@
         <div 
           class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8"
         >
-
+        <InquiryItem />
         </div>
       </div>
     </div>
