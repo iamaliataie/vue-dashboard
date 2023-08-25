46f3fc395bd13f65e9baad2d1dc8ba6b2f3af3f0
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2023-08-25 07:18:06 +0430

    update inquiries view page

--- src/views/InquiryView.vue
+++ src/views/InquiryView.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="">
-    <h1 class="text-3xl font-bold">Inquiry page</h1>
-  </div>
+    <div>
+      <h1 class="text-3xl font-bold">Inquiries</h1>
+      <div class="py-2 rounded-md">
+        <div 
+          class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8"
+        >
+          
+        </div>
+      </div>
+    </div>
 </template>
 
 <style scoped>
