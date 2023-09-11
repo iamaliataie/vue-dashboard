c8bdfa216dc075554e7eca77a288187cb22ce534
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-09-11 13:47:53 +0430

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,8 +7,9 @@ const props = defineProps(['isExpanded', 'toggleSidebar']);
 <template>
     <div class="bg-gray-300 w-full sticky top-0 z-10">
         <div class="flex flex-row-reverse justify-start gap-x-2 items-center h-[50px] w-full px-4">
-            <span class="md:hidden material-symbols-outlined cursor-pointer"
-            @click="toggleSidebar"
+            <span 
+                class="md:hidden material-symbols-outlined cursor-pointer"
+                @click="toggleSidebar"
             >
                 {{ isExpanded ? 'close': 'menu' }}
             </span>
