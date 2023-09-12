f2714d87399d7e73cf9a3a0e1e7b493f88b04b97
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-09-12 11:45:20 +0430

    Update InquiryView.vue

--- src/views/InquiryView.vue
+++ src/views/InquiryView.vue
@@ -6,9 +6,7 @@ import InquiryItem from '../components/InquiryItem.vue';
     <div>
       <h1 class="text-3xl font-bold">Inquiries</h1>
       <div class="py-2 rounded-md">
-        <div 
-          class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8"
-        >
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         <InquiryItem />
         </div>
       </div>
