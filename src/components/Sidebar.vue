02fdffa1491bf3ec72baea45dcbf88612348a10f
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2023-09-22 13:23:01 +0430

    update sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -12,6 +12,7 @@ const props = defineProps(['isExpanded', 'toggleSidebar'])
 
 const handleLogout = () => {
     userStore.handleLogout()
+    props.toggleSidebar()
     router.push({name: 'login'})
 }
 
