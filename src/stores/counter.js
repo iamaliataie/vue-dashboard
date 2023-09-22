d6f261fb24afd901d33a7b929af19a93912fa7dc
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2023-09-22 12:45:16 +0430

    add message in user store

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -15,9 +15,13 @@ export const useUserStore = defineStore('user', () => {
   const handleLogin = (credentials) => {
     const { email, password } = credentials
 
+    message.value = ''
+
     if (email == user.email && password == user.password) {
       return authenticated.value = true;
     }
+
+    message.value = 'Invalid credentials'
 
   }
 
