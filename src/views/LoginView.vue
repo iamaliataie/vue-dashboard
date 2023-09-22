adcde5ee6d6a8486206ed329302e7ac283563b80
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2023-09-22 13:05:30 +0430

    update loginview

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -10,12 +10,16 @@ const credentials = reactive({
     password: ''
 })
 
+const handleSubmit = () => {
+    
+}
+
 </script>
 <template>
     <div class="h-full flex justify-center items-center">
         <div class="bg-emerald-600 rounded-md p-6 w-full md:w-[500px]">
             <h1 class="text-center text-3xl font-bold  text-white">Login</h1>
-            <div class="flex flex-col space-y-4 mt-4">
+            <form @submit.prevent="handleSubmit" class="flex flex-col space-y-4 mt-4">
                 <div>
                     <label for="" class="text-lg text-white">Email</label>
                     <input type="email" v-model="credentials.email" class="block w-full p-3 rounded text-xl focus:outline-none">
@@ -25,7 +29,7 @@ const credentials = reactive({
                     <input type="password" v-model="credentials.password" class="block w-full p-3 rounded text-xl focus:outline-none">
                 </div>
                 <button type="submit" class="bg-emerald-400 rounded-md px-3 py-3 text-white hover:bg-emerald-500 ease-linear duration-100 font-semibold">Login</button>
-            </div>
+            </form>
             <p class="text-md mt-3 text-gray-200">Forgot your password? <a href="#" class="text-gray-600 underline">click here</a></p>
         </div>
     </div>
