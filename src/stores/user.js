fa4539d21cf3005e2720da99baaf08634d4d4c14
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-09-24 13:17:35 +0430

    Update user.js

--- src/stores/user.js
+++ src/stores/user.js
@@ -4,13 +4,11 @@ import { defineStore } from 'pinia'
 export const useUserStore = defineStore('user', () => {
   
   const authenticated = ref(false)
-
+  const message = ref('')
   const user = reactive({
     email: 'test@test.com',
     password: 'testuser'
   })
-
-  const message = ref('')
 
   const handleLogin = (credentials) => {
     const { email, password } = credentials
