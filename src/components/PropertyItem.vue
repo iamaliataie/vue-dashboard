72d84a5d98a3088c8f16fc8d28e8d4980e75ca26
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2023-09-30 07:39:35 +0430

    update propertyview with props

--- src/components/PropertyItem.vue
+++ src/components/PropertyItem.vue
@@ -6,17 +6,20 @@ const props = defineProps(['property'])
 <template>
     <div class="bg-white overflow-hidden rounded-lg p-2 flex flex-col space-y-4 shadow-xl border">
         <div class="w-full relative overflow-hidden rounded-sm bg-green-400">
-            <img src="../assets/bookmark.png" alt="" class="w-full">
+            <img :src="property.image" alt="" class="w-full">
         </div>
         <div class="w-full flex flex-col space-y-4">
-            <small class="bg-red-500 text-white px-2 py-1 rounded-md text-lg w-fit">Commercial</small>
+            <small class="text-white px-2 py-1 rounded-md text-lg w-fit" :class="property.color">
+                {{ property.type }}
+            </small>
             <div>
-                <h1 class="text-3xl">House One</h1>
-                <p class="text-2xl">400,000 AFN</p>
+                <h1 class="text-3xl">{{ property.title }}</h1>
+                <p class="text-2xl">{{ property.price }} AFN</p>
             </div>
         </div>
         <div class="w-full">
-            <a href="#" class="block py-2 px-3 bg-blue-600 text-white text-xl lg:text-2xl text-center rounded-md">View Property</a>
+            <a href="#" class="block py-2 px-3 bg-blue-600 text-white text-xl lg:text-2xl text-center rounded-md">View
+                Property</a>
         </div>
     </div>
 </template>
