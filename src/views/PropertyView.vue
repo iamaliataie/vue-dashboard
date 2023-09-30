6cafc7e4eda684dbb3676a3f3c5809cab122bf19
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2023-09-30 07:36:57 +0430

    pass property to propertyitemv.vue

--- src/views/PropertyView.vue
+++ src/views/PropertyView.vue
@@ -9,14 +9,14 @@ const properties = propertyStore.properties
 </script>
 
 <template>
-  <div>
-    <h1 class="text-3xl font-bold">Property page</h1>
-    <div class="py-2 rounded-md">
-      <div 
-        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8"
-      >
-        <PropertyItem />
+    <div>
+      <h1 class="text-3xl font-bold">Property page</h1>
+      <div class="py-2 rounded-md">
+        <div 
+          class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8"
+        >
+          <PropertyItem v-for="(property, index) in properties" :key="index" :property="property" />
+        </div>
       </div>
     </div>
-  </div>
 </template>
